d59b4c9c8d02c0f8276455118ac825d1bfbe4647
Igor <igorcossta21@gmail.com>
2022-10-14 17:01:51 -0300

    :building_construction: add communication with client side load balancing

--- microservice-1/src/main/java/io/github/igorcossta/microservice1/Microservice1Application.java
+++ microservice-1/src/main/java/io/github/igorcossta/microservice1/Microservice1Application.java
@@ -3,6 +3,7 @@ package io.github.igorcossta.microservice1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @RestController
 public class Microservice1Application {
+    private final Environment environment;
+
+    public Microservice1Application(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("microservice1-endpoint")
     public String microservice1() {
-        return "I am a REST API in microservice 1";
+        return "I am a REST API in microservice 1 at port " + environment.getProperty("local.server.port");
     }
 
     public static void main(String[] args) {
--- microservice-2/src/main/java/io/github/igorcossta/microservice2/Microservice2Application.java
+++ microservice-2/src/main/java/io/github/igorcossta/microservice2/Microservice2Application.java
@@ -2,32 +2,28 @@ package io.github.igorcossta.microservice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class Microservice2Application {
-    private final DiscoveryClient discoveryClient;
-
-    public Microservice2Application(DiscoveryClient discoveryClient) {
-        this.discoveryClient = discoveryClient;
-    }
 
     @GetMapping("microservice2-endpoint")
     public String microservice2() {
-        List<ServiceInstance> instances = discoveryClient.getInstances("microservice1");
-        ServiceInstance serviceInstance = instances.get(0);
-        String url = serviceInstance.getUri() + "/microservice1-endpoint";
-        RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(url, String.class);
+        String url = "http://microservice1/microservice1-endpoint";
+        return restTemplate().getForObject(url, String.class);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
